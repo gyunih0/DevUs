b5d6e3ab1da6a02c078cef07d90fd029470956aa
Hogyun Kim <ggyuniH0@gmail.com>
2022-05-10 11:57:44 +0900

    로그인/ ì회원가입 구현

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 import hashlib
-
+import jwt
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
 import certifi
 
+SECRET_KEY = 'DEVUS'
+
 ca = certifi.where()
 client = MongoClient('mongodb+srv://test:sparta@cluster0.7y6m3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.devus
+
 
 @app.route('/')
 def main():
     return render_template("main.html")
 
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
+'''
+회원 가입 API
+'''
 
-
+# HTML rendering
 @app.route('/join')
 def join():
     return render_template("join.html")
 
 
+# 회원 등록
 @app.route('/join/save', methods=['POST'])
 def join_save():
     name_receive = request.form['name_give']
@@ -39,22 +44,48 @@ def join_save():
     return jsonify({'result': 'success'})
 
 
+# ID 중복 체크
 @app.route('/join/check_dup', methods=['POST'])
 def check_dup():
     id_receive = request.form['id_give']
-    exists = bool(db.users.find_one({"id": id}))
+    exists = bool(db.user.find_one({"id": id_receive}))
+
     return jsonify({'result': 'success', 'exists': exists})
 
 
-@app.route('/detail/<keyword>')
-def detail(keyword):
-    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token c1764068a2906e62e159903ec6d136d91e7ff0a6"})
-    result = r.json()
+
+'''
+로그인 API
+'''
+
+# HTML rendering
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+# 로그인 기능
+@app.route('/login', methods=['POST'])
+def sign_in():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
     print(result)
 
-    word_receive = request.args.get("word_give")
-    print(word_receive)
-    return render_template("detail.html", word=keyword)
+    if result is not None:
+        payload = {
+         'id': id_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})  # deconde('utf-8') 오류
+
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 if __name__ == '__main__':
